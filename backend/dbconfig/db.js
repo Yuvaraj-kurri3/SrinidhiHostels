c677c4519fe9a4f5fdd62f7cd81649c09c734366
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2025-12-25 10:46:40 +0530

    .envvv

--- backend/dbconfig/db.js
+++ backend/dbconfig/db.js
@@ -1,12 +1,16 @@
 import mongodb from 'mongodb';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 
 async function connectDB() {
- try {
+  try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully✅');
  } catch (error) {
